2bf4784968b817c833e06f6aa05d5b2318058a04
50l3r <500l3r@gmail.com>
2020-07-21 11:48:09 +0200

    Initial commit

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
 module.exports = {
-    extends: [
+	parser: 'babel-eslint',
+	env: {
+		browser: true,
+		es6: true,
+	},
+	extends: [
 		'eslint:recommended',
-		'plugin:vue/recommended',
-    ],
-    rules: {
-		'no-console': [process.env.NODE_ENV === 'production' ? 2 : 0, {allow: ["warn", "error"] }],
-		'indent': [2, "tab"],
+	],
+	rules: {
+		'indent': [2, 'tab'],
+		'semi': [2, 'always'],
 		'object-curly-newline': 0,
-		'key-spacing': [2, {"beforeColon": true, "afterColon": true }],
-		///////////////////////
-		// VUE
-		///////////////////////
-		'vue/name-property-casing': [2, 'PascalCase'],
-		'vue/prop-name-casing': [2, 'camelCase'],
-		'vue/max-attributes-per-line': 0,
-		'vue/require-default-prop': 0,
-		'vue/component-name-in-template-casing': [2, 'kebab-case', {registeredComponentsOnly: false}],
-		'vue/html-indent': [2, 'tab', {
-			attribute: 1,
-			baseIndent: 1,
-			closeBracket: 0,
-			alignAttributesVertically: true,
-			ignores: [],
-		}]
-    }
+		'key-spacing': [2, { "beforeColon": false, "afterColon": true }]
+	},
 };
---
+++ vue.js
@@ -0,0 +1,20 @@
+module.exports = {
+	extends: [
+		'plugin:vue/recommended',
+		'@vue/airbnb',
+	],
+	rules: {
+		'vue/name-property-casing': [2, 'PascalCase'],
+		'vue/prop-name-casing': [2, 'camelCase'],
+		'vue/max-attributes-per-line': 0,
+		'vue/require-default-prop': 0,
+		'vue/component-name-in-template-casing': [2, 'kebab-case', { registeredComponentsOnly: false }],
+		'vue/html-indent': [2, 'tab', {
+			attribute: 1,
+			baseIndent: 1,
+			closeBracket: 0,
+			alignAttributesVertically: true,
+			ignores: [],
+		}]
+	}
+};

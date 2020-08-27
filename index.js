a404f2fa91af1680bb4b684fe8d527c7164d7190
50l3r <500l3r@gmail.com>
2020-08-27 11:57:11 +0200

    Añadido de reglas y configuraciones
    - Eliminado Prettier
    - Añadidas reglas
    - Cambiado AirBNB

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 module.exports = {
 	parser: 'babel-eslint',
 	env: {
-		browser: true,
-		es6: true,
+		node: true,
+		es6: true
 	},
-	"extends": ["airbnb"],
+	extends: ['airbnb-base'],
 	rules: {
-		'no-console': [1, { "allow": ["warn", "error"] }],
-		'indent': [1, 'tab'],
-		'semi': [1, 'always'],
+		'no-console': [1, { allow: ['warn', 'error'] }],
+		'no-tabs': 0,
+		'comma-dangle': [1, 'never'],
 		'object-curly-newline': 0,
-		'key-spacing': [2, { "beforeColon": false, "afterColon": true }]
+		'key-spacing': [2, { beforeColon: false, afterColon: true }],
+		'no-underscore-dangle': 0,
+		'prefer-destructuring': 0,
+		'func-names': 0,
+		'import/extensions': [
+			2,
+			{
+				js: 'never'
+			}
+		],
+		indent: [1, 'tab', { SwitchCase: 1 }],
+		semi: [1, 'always']
 	},
+	settings: {
+		'import/resolver': {
+			alias: {
+				map: [['@', './src']],
+				extensions: ['.js']
+			}
+		}
+	}
 };
--- vue.js
+++ vue.js
@@ -1,19 +1,30 @@
 module.exports = {
-	extends: [
-		'plugin:vue/recommended'
-	],
+	extends: ['plugin:vue/recommended'],
 	rules: {
+		'no-param-reassign': 0, // VUEX
+		'no-shadow': 0, // VUEX
 		'vue/name-property-casing': [2, 'PascalCase'],
 		'vue/prop-name-casing': [2, 'camelCase'],
-		'vue/max-attributes-per-line': 0,
+		// 'vue/max-attributes-per-line': 0,
 		'vue/require-default-prop': 0,
 		'vue/component-name-in-template-casing': [2, 'kebab-case', { registeredComponentsOnly: false }],
-		'vue/html-indent': [2, 'tab', {
-			attribute: 1,
-			baseIndent: 1,
-			closeBracket: 0,
-			alignAttributesVertically: true,
-			ignores: [],
-		}]
+		'vue/html-indent': [
+			2,
+			'tab',
+			{
+				attribute: 1,
+				baseIndent: 1,
+				closeBracket: 0,
+				alignAttributesVertically: true,
+				ignores: []
+			}
+		],
+		'import/extensions': [
+			2,
+			{
+				js: 'never',
+				vue: 'never'
+			}
+		]
 	}
 };

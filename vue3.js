bb1819cdafc02078c8a89c1c9b66fafcaeaa4e0e
50l3r <500l3r@gmail.com>
2021-01-13 14:37:56 +0100

    Add vue3 config

---
+++ vue3.js
@@ -0,0 +1,37 @@
+module.exports = {
+	extends: ['plugin:vue/vue3-recommended'],
+	rules: {
+		'no-param-reassign': 0, // VUEX
+		'no-shadow': 0, // VUEX
+		'vue/name-property-casing': [2, 'PascalCase'],
+		'vue/prop-name-casing': [2, 'camelCase'],
+		// 'vue/max-attributes-per-line': 0,
+		'vue/require-default-prop': 0,
+		'vue/component-name-in-template-casing': [2, 'kebab-case', { registeredComponentsOnly: false }],
+		'vue/html-indent': [
+			2,
+			'tab',
+			{
+				attribute: 1,
+				baseIndent: 1,
+				closeBracket: 0,
+				alignAttributesVertically: true,
+				ignores: []
+			}
+		],
+		'import/extensions': [
+			2,
+			{
+				js: 'never',
+				vue: 'never'
+			}
+		]
+	},
+	settings: {
+		'import/resolver': {
+			alias: {
+				extensions: ['.js', '.vue', '.scss', '.less']
+			}
+		}
+	}
+};
